4b906c7b148ea9c06cf0d4eaf6be9ca9e9258e36
ryoryoryoooo <P1211065@gmail.com>
2021-11-23 07:16:29 +0900

    first commit

---
+++ muranakaKadai19/FruitsRepository.swift
@@ -0,0 +1,22 @@
+//
+//  UserDefaultRepository.swift.swift
+//  muranakaKadai19
+//
+//  Created by 村中令 on 2021/11/21.
+//
+
+import UIKit
+
+class FruitsRepository {
+    static func setFruits(fruits: [Fruit]) {
+        let data = try? JSONEncoder().encode(fruits)
+        UserDefaults.standard.set(data, forKey: "persons")
+    }
+
+    static func readFruits() -> [Fruit]? {
+        let data = UserDefaults.standard.data(forKey: "persons")
+        guard let data = data else { return nil }
+        let persons = try? JSONDecoder().decode(Array<Fruit>.self, from: data)
+        return persons
+    }
+}
